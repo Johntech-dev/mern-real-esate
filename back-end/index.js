b316e70f0f00f04d0e7cdb5a7302cccc63d99356
Akinmolayan john <johnayomide50@gmail.com>
2023-11-21 03:16:16 -0800

    chanages

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config()
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log("Connected to mongodb")
+mongoose.connect("mongodb+srv://johnayomide50:Matthew12@mern-real-estate.tpul0gd.mongodb.net/?retryWrites=true&w=majority").then(() => {
+    console.log("Connected to mongodb");
 }) .catch ((err) => {
     console.log("an error occured");
 })
@@ -12,5 +12,5 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 
 app.listen(3000, () => {
-    console.log("server is running on port 5000")
+    console.log("server is running on port 3000")
 })

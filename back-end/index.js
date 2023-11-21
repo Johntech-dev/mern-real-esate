b978b240fd6b300ac4cf890774389cf0561fb6e4
Akinmolayan john <johnayomide50@gmail.com>
2023-11-20 18:48:24 -0800

    updating connection to mongodb

--- back-end/index.js
+++ back-end/index.js
@@ -1,17 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
-
+import dotenv from 'dotenv'
+dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(() => {
-    console.log("connected to db")
+    console.log("Connected to mongodb")
 }) .catch ((err) => {
-    console.log("an error occured")
+    console.log("an error occured");
 })
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("server running on port 2000");
+    console.log("server is running on port 3000")
 })

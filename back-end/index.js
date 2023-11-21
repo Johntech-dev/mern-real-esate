a2d648bf280fde784e111d33a351fc8c69fba3ba
Akinmolayan john <johnayomide50@gmail.com>
2023-11-20 18:39:57 -0800

    Connected to the database

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,17 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("connected to db")
+}) .catch ((err) => {
+    console.log("an error occured")
+})
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("server running on port 3000");
+    console.log("server running on port 2000");
 })

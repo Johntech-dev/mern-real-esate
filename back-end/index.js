758fa57229b8bcfcaa3c9784491e1bdb779ae7c5
Akinmolayan john <johnayomide50@gmail.com>
2023-11-21 04:14:20 -0800

    created a test api route

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+
 dotenv.config()
 
 mongoose.connect("mongodb+srv://johnayomide50:Matthew12@mern-real-estate.tpul0gd.mongodb.net/?retryWrites=true&w=majority").then(() => {
@@ -14,3 +16,5 @@ const app = express();
 app.listen(3000, () => {
     console.log("server is running on port 3000")
 })
+
+app.use('/back-end/user', userRouter)
